b0067869726261e23df3da1e0969f5313fcc24c0
sohail2000 <msohail1379@gmail.com>
2022-06-27 01:21:36 +0530

    dashboard-app

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3 w-${width} hover:drop-shadow-xl`}
+    >
+     {text}
+    </button>
+  )
+}
+
+export default Button
